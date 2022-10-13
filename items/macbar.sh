81600db29fc42f6f27a3e84c9ae0d41f6e93b400
mudong <feng2r200@qq.com>
2022-10-13 19:19:56 +0800

    fix: fix icon sort

--- items/macbar.sh
+++ items/macbar.sh
@@ -17,15 +17,6 @@ sketchybar --add alias TextInputMenuAgent right                 \
                                     background.padding_left=4   \
                                     background.padding_right=4
 
-sketchybar --add alias WeChat right                 \
-           --set WeChat icon.padding_left=-8        \
-                        icon.padding_right=-8       \
-                        label.padding_left=-8       \
-                        label.padding_right=-8      \
-                        background.height=2         \
-                        background.padding_left=2   \
-                        background.padding_right=2
-
 sketchybar --add alias DingTalk right                 \
            --set DingTalk icon.padding_left=-8        \
                           icon.padding_right=-8       \
@@ -34,4 +25,13 @@ sketchybar --add alias DingTalk right                 \
                           background.height=2         \
                           background.padding_left=2   \
                           background.padding_right=2
+
+sketchybar --add alias WeChat right                 \
+           --set WeChat icon.padding_left=-8        \
+                        icon.padding_right=-8       \
+                        label.padding_left=-8       \
+                        label.padding_right=-8      \
+                        background.height=2         \
+                        background.padding_left=2   \
+                        background.padding_right=2
 
